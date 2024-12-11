f7b1019c714d7bda812d2c12a27b14124b114d71
madisontanle <46909580+madisontanle@users.noreply.github.com>
2024-12-11 20:28:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,13 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate     
 
 # Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Monika/g' package/base-files/files/bin/config_generate
+
+sed -i 's/^.*optionssid .*/        optionssid Monika/' /package/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/^.*option key .*/        option key monika111/' /package/mac80211/files/lib/wifi/mac80211.sh
